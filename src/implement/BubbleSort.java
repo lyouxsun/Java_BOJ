a674b49450799b183df6cb7a118ef0e3f217ad74
lyouxsun <leedrkr323@konkuk.ac.kr>
2023-06-21 18:34:03 +0900

    Add BubbleSort - BOJ1137

---
+++ src/implement/BubbleSort.java
@@ -0,0 +1,34 @@
+package implement;
+
+
+import java.util.Scanner;
+
+public class BubbleSort {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        StringBuilder sb = new StringBuilder();
+        int input = sc.nextInt();       //정렬할 원소의 개수
+        int[] arr = new int[input];
+        for(int i=0;i<input;i++){
+            arr[i]=sc.nextInt();
+        }
+
+        for(int i=1;i<input;i++){
+            for(int j=0;j<input-i;j++){
+                if(arr[j]>arr[j+1]){
+                    swap(arr, j, j+1);
+                }
+            }
+        }
+        for(int i=0;i<input;i++){
+            sb.append(arr[i]+"\n");
+        }
+        System.out.println(sb);
+    }
+
+    private static void swap(int[] arr, int j, int i) {
+        int tmp=arr[j];
+        arr[j]=arr[i];
+        arr[i]=tmp;
+    }
+}
